6937342f5fd0ef90347c5399077c2bba91cee116
Mohamed Hesham <mohamedhesham1619@gmail.com>
2022-09-28 21:45:40 +0200

    feat: add '/api/fileanalyse' endpoint to respond with meta data of the uploaded file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
 
 var app = express();
 
@@ -11,6 +12,13 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse',multer({ dest: 'uploads/' }).single('upfile') , (req, res)=>{
+    res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
 
